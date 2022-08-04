0f605a8d648dfec57f0b6fd4b96dacc9065e9a09
tigran.tskanyan <tigran.tskanyan@spbtv.com>
2022-08-04 11:24:54 +0300

    initial commit of vue config

---
+++ src/components/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="plusCount">{{ count }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      count: 101,
+    };
+  },
+  methods: {
+    plusCount() {
+      this.count += 1;
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  body {
+    color: red;
+  }
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,134 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+// const autoprefixer = require("autoprefixer");
+
+const isProd = process.env.NODE_ENV === "production";
+const isDev = !isProd;
+
+const optimization = () => {
+  const config = {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          priority: -10,
+          chunks: "all",
+        },
+      },
+    },
+  };
+
+  if (isProd) {
+    config.minimizer = [
+      new CssMinimizerWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
+
+  return config;
+};
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.ts"),
+  mode: isProd ? "production" : "development",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[contenthash:8].js",
+    chunkFilename: "[name].[contenthash:8].js",
+  },
+  devtool: isDev ? "source-map" : false,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
+        loader: "file-loader",
+        options: {
+          name: "[name][contenthash:8].[ext]",
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name][contenthash:8].[ext]",
+          outputPath: "assets/img",
+          esModule: false,
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash:8].css",
+      chunkFilename: "[name].[contenthash:8].css",
+    }),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "src", "public", "index.html"),
+      filename: isDev ? "index.html" : "start-page.html",
+      alwaysWriteToDisk: true,
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    alias: {
+      vue: "@vue/runtime-dom",
+    },
+    extensions: ["*", ".js", ".json", ".ts", ".vue"],
+  },
+  optimization: optimization(),
+  devServer: {
+    compress: true,
+    port: 9000,
+    hot: true,
+    static: {
+      directory: path.resolve(__dirname, "build"),
+    },
+    client: {
+      overlay: true,
+    },
+  },
+};
